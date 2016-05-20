fbcaca07822aa659437233c43c2e21f10d81aa9a
NealCloud <nealcloud@gmail.com>
2016-05-20 15:34:38 -0700

    update menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 
 
     // Menu settings
-$('#menuToggle, .menu-close').on('click', function(){
-    $('#menuToggle').toggleClass('active');
-    $('body').toggleClass('body-push-toleft');
-    $('#theMenu').toggleClass('menu-open');
-});
+//$('#menuToggle, .menu-close').on('click', function(){
+//    $('#menuToggle').toggleClass('active');
+//    $('body').toggleClass('body-push-toleft');
+//    $('#theMenu').toggleClass('menu-open');
+//});
 
 var screenWidth = 500;
 
